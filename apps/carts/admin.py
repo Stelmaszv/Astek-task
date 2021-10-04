5b4c8bde20622ed9f1c9feeb6bd1e2bbbfbf4683
= <stelmaszv@gmail.com>
2021-10-04 14:20:11 +0200

    Add model "Dish" (carts.models.py)

--- apps/carts/admin.py
+++ apps/carts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Cart
+from .models import Cart,Dish
 admin.site.register(Cart)
+admin.site.register(Dish)
 # Register your models here.
--- apps/carts/models.py
+++ apps/carts/models.py
@@ -2,7 +2,22 @@ from django.db import models
 
 class Cart(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100,unique=True)
+    name = models.CharField(max_length=200,unique=True)
     description = models.TextField(null=True)
     added = models.DateTimeField(auto_now=True)
     last_update=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Dish(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits = 5,decimal_places = 2)
+    preparation_time = models.BigIntegerField(default=0)
+    added = models.DateTimeField(auto_now=True)
+    last_update = models.DateTimeField(auto_now=True)
+    is_vege = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
