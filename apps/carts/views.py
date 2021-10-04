3f26ca568cf8e87e5eb89140a066907652d61a98
= <stelmaszv@gmail.com>
2021-10-04 12:20:18 +0200

    Fix class "APIPrototype" and add class "CartList" (carts.view.py)

--- apps/carts/views.py
+++ apps/carts/views.py
@@ -1,15 +1,41 @@
 from rest_framework.views import  APIView
 from rest_framework import status
 from rest_framework.response import Response
+from .serializers import CartSerializer
+from .models import Cart
 
-class APIPrototypeGet(APIView):
+class APIPrototype(APIView):
 
-    SerializerClass=None
-    queryset=None
+    SerializerClass = None
+    many     = True
+    queryset = ''
+    order_by = ''
+
+    def list(self):
+        serializer = self.SerializerClass(self.queryset, many=self.many)
+        if len(self.order_by):
+            list = sorted(
+                serializer.data,
+                key=lambda tup: tup[self.order_by],
+                reverse=True)
+        else:
+            list= serializer.data
+        return list
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.SerializerClass(data=request.data, many=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=self.list(), status=status.HTTP_201_CREATED)
+        return self.api_get(request)
+
+    def api_get(self, request, *args, **kwargs):
+        return Response(data=self.list(), status=status.HTTP_200_OK)
 
     def get(self, request, *args, **kwargs):
         return self.api_get(request)
 
-    def api_get(self, request, *args, **kwargs):
-        serializer = self.SerializerClass(self.queryset)
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+class CartList (APIPrototype):
+    SerializerClass  = CartSerializer
+    queryset          = Cart.objects
+    order_by          = 'avg_rating'
