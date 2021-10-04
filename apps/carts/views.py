4ce75c331899323d6596ab507d2229b0f98f389d
= <stelmaszv@gmail.com>
2021-10-04 12:17:03 +0200

    Add class APIPrototypeGet (carts.views.py)

--- apps/carts/views.py
+++ apps/carts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import  APIView
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+class APIPrototypeGet(APIView):
+
+    SerializerClass=None
+    queryset=None
+
+    def get(self, request, *args, **kwargs):
+        return self.api_get(request)
+
+    def api_get(self, request, *args, **kwargs):
+        serializer = self.SerializerClass(self.queryset)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
